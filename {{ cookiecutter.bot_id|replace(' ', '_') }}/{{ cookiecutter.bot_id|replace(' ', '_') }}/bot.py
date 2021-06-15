32cc894ea105e1282dba4ead1a75e209fcb1cc01
Hugo Slepicka <hhslepicka@gmail.com>
2021-06-15 16:49:18 -0700

    ENH: Update template to include example for add_image and integration with BotMaestro.

--- {{ cookiecutter.bot_id|replace(' ', '_') }}/{{ cookiecutter.bot_id|replace(' ', '_') }}/bot.py
+++ {{ cookiecutter.bot_id|replace(' ', '_') }}/{{ cookiecutter.bot_id|replace(' ', '_') }}/bot.py
@@ -6,6 +6,14 @@ class Bot(DesktopBot):
         # Opens the BotCity website.
         self.browse("https://botcity.dev/en")
 
+        # add an image for search
+        # self.add_image("start", self.get_resource_abspath("start.png"))
+
+        # Post an artifact into BotMaestro
+        # self.maestro.post_artifact(
+        #     execution.task_id, "artifact_name", "artifact_path.xlsx"
+        # )
+
 
 if __name__ == '__main__':
     Bot.main()
