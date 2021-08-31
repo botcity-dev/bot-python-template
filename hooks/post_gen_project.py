ac0d2ca1ddc0965aad2ee40e460cb4d7668b9206
Hugo Slepicka <hhslepicka@gmail.com>
2021-08-30 23:35:06 -0700

    ENH: Add option for both Web and Desktop Bot on the same Bot.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,8 +2,19 @@ import os
 
 project_type = "{{cookiecutter.project_type | lower}}"
 
-if project_type == "web":
-    os.remove(os.path.join("{{cookiecutter.bot_id}}", "bot.py"))
-    os.rename(os.path.join("{{cookiecutter.bot_id}}", "webbot.py"), os.path.join("{{cookiecutter.bot_id}}", "bot.py"))
-else:
-    os.remove(os.path.join("{{cookiecutter.bot_id}}", "webbot.py"))
+proj_files = {
+    "desktop": {"remove": ["webbot.py", "bothbot.py"], "use": None},
+    "web": {"remove": ["bot.py", "bothbot.py"], "use": "webbot.py"},
+    "both": {"remove": ["bot.py", "webbot.py"], "use": "bothbot.py"}
+}
+
+config = proj_files.get(project_type)
+
+if not config:
+    raise ValueError(f"Invalid project type: {project_type}")
+
+for f in config["remove"]:
+    os.remove(os.path.join("{{cookiecutter.bot_id}}", f))
+
+if config["use"]:
+    os.rename(os.path.join("{{cookiecutter.bot_id}}", config["use"]), os.path.join("{{cookiecutter.bot_id}}", "bot.py"))
