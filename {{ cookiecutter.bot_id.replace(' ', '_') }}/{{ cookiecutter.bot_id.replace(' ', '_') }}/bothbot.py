48e5cc39c106967643aec95da22a20f7f7bdcf81
Hugo Slepicka <hhslepicka@gmail.com>
2022-07-07 09:47:08 -0700

    DOC: Add warning to install project on the correct interpreter.

--- {{ cookiecutter.bot_id.replace(' ', '_') }}/{{ cookiecutter.bot_id.replace(' ', '_') }}/bothbot.py
+++ {{ cookiecutter.bot_id.replace(' ', '_') }}/{{ cookiecutter.bot_id.replace(' ', '_') }}/bothbot.py
@@ -1,3 +1,25 @@
+"""
+WARNING:
+
+Please make sure you install the bot with `pip install -e .` in order to get all the dependencies
+on your Python environment.
+
+Also, if you are using PyCharm or another IDE, make sure that you use the SAME Python interpreter
+as your IDE.
+
+If you get an error like:
+```
+ModuleNotFoundError: No module named 'botcity'
+```
+
+This means that you are likely using a different Python interpreter than the one used to install the bot.
+To fix this, you can either:
+- Use the same interpreter as your IDE and install your bot with `pip install -e .`
+- Use the same interpreter as the one used to install the bot (`pip install -e .`)
+
+Please refer to the documentation for more information at https://documentation.botcity.dev/
+"""
+
 from botcity.web import WebBot, Browser
 from botcity.core import DesktopBot
 # Uncomment the line below for integrations with BotMaestro
