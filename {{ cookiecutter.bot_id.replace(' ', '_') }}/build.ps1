60a35da55b40c6b14e0551af60e3cf38dd89a83f
Hugo Slepicka <hhslepicka@gmail.com>
2023-07-27 14:43:29 -0700

    ENH: Add exclusion for venv folder and the bot zip file.

--- {{ cookiecutter.bot_id.replace(' ', '_') }}/build.ps1
+++ {{ cookiecutter.bot_id.replace(' ', '_') }}/build.ps1
@@ -1,1 +1,3 @@
-Compress-Archive -LiteralPath . -DestinationPath "{{ cookiecutter.bot_id|replace(' ', '_') }}.zip" -Force
+$exclude = @("venv", "{{ cookiecutter.bot_id|replace(' ', '_') }}.zip")
+$files = Get-ChildItem -Path . -Exclude $exclude
+Compress-Archive -Path $files -DestinationPath "{{ cookiecutter.bot_id|replace(' ', '_') }}.zip" -Force
